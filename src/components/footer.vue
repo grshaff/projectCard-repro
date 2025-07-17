4714fdedcce5c9402b51e2d6b25444aa1767080c
grshaff <gshaffaat@gmail.com>
2025-07-17 18:52:54 +0700

    Feat: fix footer bg color and icon

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,5 +1,5 @@
 <template>
-    <footer class="bg-gray-900 text-white p-4 text-center font-roboto font-light justify-between flex md:p-5 lg:p-6">
+    <footer class="bg-black text-white p-4 text-center font-roboto font-light justify-between flex md:p-5 lg:p-6">
     <p class="mx-2 text-[12px] text-center my-auto lg:text-[16px]">2025© {{ '</grshaff>' }}</p>
     <div class="mx-2 flex gap-x-3 my-auto">
         <a href="#"><img class="w-6 hover:opacity-80" src="../assets/icons/github.png" alt=""></a>
