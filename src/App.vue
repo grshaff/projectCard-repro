337cdae491318ead0f505f2ac7831097ce83869c
grshaff <gshaffaat@gmail.com>
2025-07-19 13:29:15 +0700

    Feat: Navbar onscroll effect

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,24 @@
 <script setup>
-import { ref, computed, watchEffect } from 'vue'
+import { ref, computed, watchEffect, onMounted, onUnmounted  } from 'vue'
 import navBar from './components/navBar.vue'
 import FooterD from './components/footer.vue'
 import Home from './page/main.vue'
 import About from './page/about.vue'
 import Blogs from './page/blogs.vue'
 // import NotFound from './page/NotFound.vue' // Optional
+const isScrolled = ref(false)
+
+const handleScroll = () => {
+  isScrolled.value = window.scrollY > 20
+}
+
+onMounted(() => {
+  window.addEventListener('scroll', handleScroll)
+})
+
+onUnmounted(() => {
+  window.removeEventListener('scroll', handleScroll)
+})
 
 const routes = {
   '/': { component: Home, title: 'Home | Grshaff Portfolio' },
@@ -34,7 +47,9 @@ watchEffect(() => {
 
 <template>
   <div class="flex flex-col min-h-screen">
-    <navBar :current-path="currentPath" />
+    <div :class="['sticky top-0 z-50 bg-white transition-opacity duration-300', isScrolled ? 'opacity-80' : 'opacity-100']">
+      <navBar :current-path="currentPath" />
+    </div>
     <main class="flex-grow">
       <component :is="currentRoute.component" />
     </main>
